2613f19789800892f764539c7b419a39c3559aca
Marco Andres <69155968+Markdree@users.noreply.github.com>
2023-08-07 18:25:22 -0400

    Add files via upload

---
+++ dash.py
@@ -0,0 +1,44 @@
+
+
+
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+
+st.title("Inversiones Finanzas")
+
+tickers = ["AAPL","MSFT","TSLA","BTC-USD","ETH-USD"]
+
+dropdown = st.multiselect("Elige tus activos", tickers)
+
+start = st.date_input('Fecha Inicial', value = pd.to_datetime("2023-01-31"))
+end = st.date_input("Fecha Final", value = pd.to_datetime("today"))
+
+st.markdown("""---""")
+
+st.markdown("""---""")
+
+
+def relativeret(df):
+    rel = df.pct_change()
+    cumret = (1+rel).cumprod()-1
+    cumret = cumret.fillna(0)
+    return cumret
+
+
+
+if len(dropdown) > 0:
+ 
+    df = relativeret(yf.download(dropdown, start, end)["Adj Close"])
+    
+    df2 = yf.download(dropdown, start, end)["Adj Close"]
+    
+    st.line_chart(df)
+
+    
+    st.markdown("""---""")
+    
+    
+    st.bar_chart(df2)
